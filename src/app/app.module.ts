9a90206371a69ff9f62687d3241da99b7c29e3ce
Minim131 <100144145+Minim131@users.noreply.github.com>
2023-03-22 15:12:07 +0800

    Added routing to learning menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ModulePanelComponent } from './module-panel/module-panel.component';
 import { TopPanelComponent } from './top-panel/top-panel.component';
 import { LnaContentComponent } from './lna-content/lna-content.component';
 import { FooterComponent } from './footer/footer.component';
+import { LearningMenuComponent } from './learning-menu/learning-menu.component';
 
 import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap'
 import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+const appRoutes: Routes = [
+  { path: 'learning-menu', component: LearningMenuComponent },
+  { path: 'lna-content', component: LnaContentComponent }
+  ]
 
 
 @NgModule({
@@ -19,14 +26,16 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
     ModulePanelComponent,
     TopPanelComponent,
     LnaContentComponent,
-    FooterComponent
+    FooterComponent,
+    LearningMenuComponent
   ],
   imports: [
     BrowserModule,
     NgbTooltipModule,
     NgbPopoverModule,
     NgbCollapseModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/learning-menu/learning-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-learning-menu',
+  templateUrl: './learning-menu.component.html',
+  styleUrls: ['./learning-menu.component.css']
+})
+export class LearningMenuComponent {
+
+}
