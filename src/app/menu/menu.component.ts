2fcbf5e5c513a253562660c200d32c1f9ce6e5bb
Minim131 <100144145+Minim131@users.noreply.github.com>
2023-04-20 11:21:24 +0800

    Standardised modal menu page height
    Menu component

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -6,7 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
+  public hideTransaction = false;
+  public hideReport = true;
+  public hideSetting = true;
+  showTransaction() {
+    this.hideTransaction = false;
+    this.hideReport = true;
+    this.hideSetting = true;
+  }
+  showReport() {
+    this.hideTransaction = true;
+    this.hideReport = false;
+    this.hideSetting = true;
+  }
   showSetting() {
-   /* this.router.navigate(['setting'], { relativeTo: this.route });*/
+    this.hideTransaction = true;
+    this.hideReport = true;
+    this.hideSetting = false;
+    /* this.router.navigate(['setting'], { relativeTo: this.route });*/
   }
 }
