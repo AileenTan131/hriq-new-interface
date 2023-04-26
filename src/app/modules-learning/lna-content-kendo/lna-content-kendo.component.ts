009b3b215fb7fa8c1f0194130384e1712fe5bd76
Minim131 <100144145+Minim131@users.noreply.github.com>
2023-04-26 17:45:01 +0800

    Improved kendo lna page

--- src/app/modules-learning/lna-content-kendo/lna-content-kendo.component.ts
+++ src/app/modules-learning/lna-content-kendo/lna-content-kendo.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, ViewEncapsulation, NgZone, AfterViewInit } from '@angular/core';
 import { take } from "rxjs/operators";
-import { GridComponent } from "@progress/kendo-angular-grid";
+import { GridComponent, FilterableSettings } from "@progress/kendo-angular-grid";
+import { Customers } from "./customers"
 
 @Component({
   selector: 'app-lna-content-kendo',
@@ -8,60 +9,9 @@ import { GridComponent } from "@progress/kendo-angular-grid";
   styleUrls: ['./lna-content-kendo.component.css'], encapsulation: ViewEncapsulation.None,
 })
 export class LnaContentKendoComponent implements AfterViewInit {
-  public gridData = [
-    {
-      SN: 1,
-      CompetencyType: 'Technical',
-      Skills: 'Understands internal and external customer requirements, customer needs and organisation’s service standards and takes initiative to provide value-added advice/ suggestions to address needs.',
-      Attachment: '',
-      CurrentRate: '3/5',
-      RequiredRate: '3/5',
-      Gap: '0',
-      Piority: '2',
-      CoursesInterested: 'My Training Plan',
-      TotalHours: '10',
-      TotalBudget: '$3000',
-    },
-    {
-      SN: 2,
-      CompetencyType: 'Technical',
-      Skills: 'Escalates suggestions on how to provide better services',
-      Attachment: '',
-      CurrentRate: '3/5',
-      RequiredRate: '4/5',
-      Gap: '-1',
-      Piority: '3',
-      CoursesInterested: 'My Training Plan',
-      TotalHours: '7',
-      TotalBudget: '$1500',
-    },
-    {
-      SN: 3,
-      CompetencyType: 'Technical',
-      Skills: 'Builds rapport with internal and external customers and maintains customers’ trust by providing prompt, effective and quality service, takes continued interest in customers’ affairs.',
-      Attachment: '',
-      CurrentRate: '3/5',
-      RequiredRate: '1/5',
-      Gap: '2',
-      Piority: '4',
-      CoursesInterested: 'My Training Plan',
-      TotalHours: '8',
-      TotalBudget: '$2200',
-    },
-    {
-      SN: 3,
-      CompetencyType: 'Technical',
-      Skills: 'Escalates suggestions on how to provide better services',
-      Attachment: '',
-      CurrentRate: '3/5',
-      RequiredRate: '4/5',
-      Gap: '-2',
-      Piority: '3',
-      CoursesInterested: 'My Training Plan',
-      TotalHours: '7',
-      TotalBudget: '$1500',
-    }
-  ];
+  public gridData: unknown[] = Customers
+ 
+  public filterMode: FilterableSettings = "menu";
 
   @ViewChild(GridComponent)
   public grid: GridComponent;
@@ -81,4 +31,17 @@ export class LnaContentKendoComponent implements AfterViewInit {
       });
   }
 
+  public isCollapsed = false;
+
+  textShow = false
+
+  
+
+/*  $("#grid").kendoGrid({
+    columns: [{
+      field: "CoursesInterested",
+      template: '<a href="\\#">#= name#</a>'
+  }],
+    dataSource: gridData
+  });*/
 }
