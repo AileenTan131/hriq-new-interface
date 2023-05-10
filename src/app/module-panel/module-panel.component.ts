904095d85de4b567a036008d69bc5f44146579df
Minim131 <100144145+Minim131@users.noreply.github.com>
2023-05-10 09:47:19 +0800

    Improved code readability in the menu modal sections

--- src/app/module-panel/module-panel.component.ts
+++ src/app/module-panel/module-panel.component.ts
@@ -26,66 +26,31 @@ export class ModulePanelComponent {
   }
 
   //Learning menu
-  public menu = 'TRANSACTIONS';
-  public hideLearningTransaction = false;
-  public hideLearningReport = true;
-  public hideLearningSetting = true;
+  public menu = 'TRANSACTIONS'
 
   showLearningTransaction() {
-    this.hideLearningTransaction = false;
-    this.hideLearningReport = true;
-    this.hideLearningSetting = true;
-    return this.menu = 'TRANSACTIONS';
+    this.menu = 'TRANSACTIONS'; 
   }
   showLearningReport() {
-    this.hideLearningTransaction = true;
-    this.hideLearningReport = false;
-    this.hideLearningSetting = true;
-    return this.menu = 'REPORTS';
+    this.menu = 'REPORTS'; 
   }
   showLearningSetting() {
-    this.hideLearningTransaction = true;
-    this.hideLearningReport = true;
-    this.hideLearningSetting = false;
-    return this.menu = 'SYSTEM    ';
-    /* this.router.navigate(['setting'], { relativeTo: this.route });*/
+    this.menu = 'SYSTEM'; 
+
   }
 
   //Time Menu
-  public hideTimeTransaction = false;
-  public hideTimeExport = true;
-  public hideTimeAttendance = true;
-  public hideTimeSetting = true;
-
   showTimeTransaction() {
-    this.hideTimeTransaction = false;
-    this.hideTimeExport = true;
-    this.hideTimeAttendance = true;
-    this.hideTimeSetting = true;
-    return this.menu = 'TRANSACTIONS';
+    this.menu = 'TRANSACTIONS';
   }
   showTimeExport() {
-    this.hideTimeTransaction = true;
-    this.hideTimeExport = false;
-    this.hideTimeAttendance = true;
-    this.hideTimeSetting = true;
-    return this.menu = 'EXPORT'
+    this.menu = 'EXPORT'
   }
-
   showTimeAttendance() {
-    this.hideTimeTransaction = true;
-    this.hideTimeExport = true;
-    this.hideTimeAttendance = false;
-    this.hideTimeSetting = true;
-    return this.menu = 'ATTENDANCE'
+    this.menu = 'ATTENDANCE'
   }
-
   showTimeSetting() {
-    this.hideTimeTransaction = true;
-    this.hideTimeExport = true;
-    this.hideTimeAttendance = true;
-    this.hideTimeSetting = false;
-    return this.menu = 'SYSTEM    ';
+   this.menu = 'SYSTEM    ';
   }
 
   //Set New Password
