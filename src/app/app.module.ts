486c1414d6d68dde0ae2a23ffdd7c6ac9f92f3e7
Minim131 <100144145+Minim131@users.noreply.github.com>
2023-05-25 15:39:50 +0800

    Added animation to the buttons using Angular Material Library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { ButtonsModule } from "@progress/kendo-angular-buttons";
 import { IconsModule } from "@progress/kendo-angular-icons";
 import { NotificationModule } from '@progress/kendo-angular-notification';
 
-
+import { MatButtonModule } from '@angular/material/button';
 
 
 
@@ -46,7 +46,8 @@ import { NotificationModule } from '@progress/kendo-angular-notification';
     ButtonsModule,
     ReactiveFormsModule,
     IconsModule,
-    NotificationModule
+    NotificationModule,
+    MatButtonModule
 /*    RouterModule.forRoot(appRoutes),*/
   ],
   providers: [],
