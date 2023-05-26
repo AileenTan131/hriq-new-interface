84ffc02932ea850c362185cd8616b4b583e2f209
Minim131 <100144145+Minim131@users.noreply.github.com>
2023-05-26 17:54:56 +0800

    Position fixed for lna content and homepages & Global System Parameters creation.

--- src/app/modules-learning/global/global.component.ts
+++ src/app/modules-learning/global/global.component.ts
@@ -1,4 +1,5 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { InputSize } from "@progress/kendo-angular-inputs"
 
 
 @Component({
@@ -7,5 +8,10 @@ import { Component} from '@angular/core';
   styleUrls: ['./global.component.css']
 })
 export class GlobalComponent {
- 
+  public checked01 = true;
+  public checked02 = true;
+  public checked03 = true;
+
+  public size: InputSize = "small"
+
 }
